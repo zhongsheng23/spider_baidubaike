9f9a2e6268fc59bf00a3dae391c9b3f78b2bab96
zhongsheng <2414192895@qq.com>
2016-06-28 01:39:36 +0800

    new file

---
+++ html_outputer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class HtmlOutputer(object):
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self,data):
+        if data is None:
+            return
+        self.datas.append(data)
+
+    def output_html(self):
+        fout = open('output.html','w')
+        fout.write("<html><head><meta charset='utf-8'></head><body><table>")
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>" % data['url'])
+            fout.write("<td>%s</td>" % data['title'].encode('utf-8'))
+            fout.write("<td>%s</td>" % data['summary'].encode('utf-8'))
+            fout.write("</tr>")
+        fout.write("</table></body></html>")
+        fout.close()
